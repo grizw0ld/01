b84bee1348cba81b3b10c97f4bd1804958a19821
Mark Grzechowiak <makgrz@gmail.com>
2013-01-22 21:57:28 -0500

    Chapter 01

---
+++ Pursue2.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML  1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+   <meta http-equiv="content-type"  
+   content="text/html; charset=utf-8" />
+   <title>Hello, World!</title>
+ </head>
+ <body>
+	<p><h3>Some Basic Information</h3></p>
+	<p>
+	<?php
+		print nl2br("<b>Name: </b>Mark G\n");
+		print nl2br("<b>Location: </b>Buffalo, NY\n");
+		print nl2br("<b>Occupation: </b>BI Developer\n");
+	?>
+	</p>
+ </body>
+ </html>
---
+++ hello3.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML  1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+   <meta http-equiv="content-type"  
+   content="text/html; charset=utf-8" />
+   <title>Hello, World!</title>
+ </head>
+ <body>
+	<p>The following page was created by PHP:
+	<?php
+		/*
+		 *Filename: hello3.php
+		 *Book reference: Script 1.5
+		 *Created by: Mark Grzechowiak
+		 */
+		//print "<span style=\"font-weight:bold;\">Hello,World!</span>";
+	?>
+	<!-- HTML comment. -->
+	</p>
+ </body>
+ </html>
